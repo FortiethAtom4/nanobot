5e4e961fc74b53a1367e386f19c9baab78346847
= <txbbrand@gmail.com>
2025-03-07 11:47:41 -0500

    cleaned up cogs

--- cogs/rank.py
+++ cogs/rank.py
@@ -1,13 +1,9 @@
-import discord, datetime, config, logging
+import discord, config, logging
 from discord.ext import commands
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='nanobot.log', encoding='utf-8', level=logging.INFO, format=config.log_formatter)
 
-
-# Use this file to add your own suite of custom commands to the bot. 
-# Feel free to edit this file as you see fit.
-# Try copy/pasting command functions from other cog files into the class below if you aren't sure where to start.
 class LevelCommands(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
@@ -23,6 +19,7 @@ class LevelCommands(commands.Cog):
         await ctx.respond(f'''```You are rank {rank + 1} out of {len(config.users)} users.
 Your current level: {user.level}
 Total XP: {user.xp_total}
+Total messages: {user.total_messages}
 Progress to next level: {user.xp_current}/{user.get_level_req()} ({round((user.xp_current)/user.get_level_req()*100,2)}%)```''')
 
 
